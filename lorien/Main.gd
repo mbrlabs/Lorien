7e51b6f694ca1a50364389e5e405a9980316d7eb
Marcus Brummer <mbrlabs7@gmail.com>
2021-05-12 01:44:25 +0200

    Removed the whole broderless window stuff...not worth it

--- lorien/Main.gd
+++ lorien/Main.gd
@@ -8,10 +8,6 @@ onready var _ui_statusbar: UIStatusbar = $UIStatusBar
 onready var _ui_titlebar: UITitlebar = $UITitlebar
 onready var _ui_toolbar: UIToolbar = $UIToolbar
 
-onready var _window_border_top: Control = $WindowBorderTop
-onready var _window_border_bottom: Control = $WindowBorderBottom
-onready var _window_border_left: Control = $WindowBorderLeft
-onready var _window_border_right: Control = $WindowBorderRight
 onready var _file_dialog: FileDialog = $FileDialog
 
 # -------------------------------------------------------------------------------------------------
@@ -19,24 +15,7 @@ func _ready():
 	VisualServer.set_default_clear_color(canvas_color)
 	_file_dialog.current_dir = Config.DEFAULT_FILE_DIALOG_PATH
 	
-	# Window borders: mouse enter/exit events
-	_window_border_top.connect("mouse_entered", self, "_on_mouse_entered_window_border")
-	#_window_border_top.connect("mouse_exited", self, "_on_mouse_exited_window_border")
-	_window_border_bottom.connect("mouse_entered", self, "_on_mouse_entered_window_border")
-	#_window_border_bottom.connect("mouse_exited", self, "_on_mouse_exited_window_border")
-	_window_border_left.connect("mouse_entered", self, "_on_mouse_entered_window_border")
-	_window_border_left.connect("mouse_exited", self, "_on_mouse_exited_window_border")
-	_window_border_right.connect("mouse_entered", self, "_on_mouse_entered_window_border")
-	_window_border_right.connect("mouse_exited", self, "_on_mouse_exited_window_border")
-	
-	# Window borders: gui events (like click and drag)
-	_window_border_top.connect("gui_input", self, "_on_top_window_border_gui_input")
-	_window_border_bottom.connect("gui_input", self, "_on_bottom_window_border_gui_input")
-	_window_border_left.connect("gui_input", self, "_on_left_window_border_gui_input")
-	_window_border_right.connect("gui_input", self, "_on_right_window_border_gui_input")
-	
 	# UI Signals
-	_ui_titlebar.connect("close_requested", self, "_on_close_requested")
 	_ui_toolbar.connect("clear_canvas", self, "_on_clear_canvas")
 	_ui_toolbar.connect("open_file", self, "_on_load_file")
 	_ui_toolbar.connect("save_file", self, "_on_save_file")
@@ -75,58 +54,9 @@ func _on_save_file(filepath: String) -> void:
 	LorienIO.save_file(filepath, _canvas._brush_strokes)
 
 # -------------------------------------------------------------------------------------------------
-func _on_close_requested() -> void:
-	# TODO: ask to save unsaved project
-	get_tree().quit()
-
-# -------------------------------------------------------------------------------------------------
-func _on_top_window_border_gui_input(event: InputEvent):
-	if Input.is_mouse_button_pressed(BUTTON_LEFT) && event is InputEventMouseMotion:
-		OS.window_position.y += event.relative.y
-		OS.window_size.y -= event.relative.y
-
-# -------------------------------------------------------------------------------------------------
-func _on_bottom_window_border_gui_input(event: InputEvent):
-	if Input.is_mouse_button_pressed(BUTTON_LEFT) && event is InputEventMouseMotion:
-		OS.window_size.y += event.relative.y
-
-# -------------------------------------------------------------------------------------------------
-func _on_left_window_border_gui_input(event: InputEvent):
-	if Input.is_mouse_button_pressed(BUTTON_LEFT) && event is InputEventMouseMotion:
-		OS.window_position.x += event.relative.x
-		OS.window_size.x -= event.relative.x
-
-# -------------------------------------------------------------------------------------------------
-func _on_right_window_border_gui_input(event: InputEvent):
-	if Input.is_mouse_button_pressed(BUTTON_LEFT) && event is InputEventMouseMotion:
-		OS.window_size.x += event.relative.x
-
-# -------------------------------------------------------------------------------------------------
-func _on_mouse_entered_window_border() -> void:
-	_canvas.disable()
-
-# -------------------------------------------------------------------------------------------------
-func _on_mouse_exited_window_border() -> void:
-	_canvas.enable()
-
-# -------------------------------------------------------------------------------------------------
 func _on_InfiniteCanvas_mouse_entered():
 	_canvas.enable()
 
 # -------------------------------------------------------------------------------------------------
 func _on_InfiniteCanvas_mouse_exited():
 	_canvas.disable()
-
-# -------------------------------------------------------------------------------------------------
-func _on_UITitlebar_window_maximized():
-	_window_border_top.hide()
-	_window_border_bottom.hide()
-	_window_border_left.hide()
-	_window_border_right.hide()
-
-# -------------------------------------------------------------------------------------------------
-func _on_UITitlebar_window_demaximized():
-	_window_border_top.show()
-	_window_border_bottom.show()
-	_window_border_left.show()
-	_window_border_right.show()
--- lorien/UI/UITitlebar.gd
+++ lorien/UI/UITitlebar.gd
@@ -5,36 +5,18 @@ class_name UITitlebar
 const UI_TAB = preload("res://UI/Components/UITab.tscn")
 
 # -------------------------------------------------------------------------------------------------
-signal close_requested
-signal window_maximized
-signal window_demaximized
-signal window_minimized
 signal file_tab_selected
 signal file_tab_close_requested
 
 # -------------------------------------------------------------------------------------------------
 onready var _file_tabs: HBoxContainer = $Left/Tabs
 export var _menu_popup_path: NodePath
-var _mouse_pressed := false
-var _mouse_draging := false
-var _drag_offset: Vector2
 var _active_file_tab: UITab
 
 # -------------------------------------------------------------------------------------------------
 func _ready() -> void:
 	var tab := add_tab("Untitled", "")
 	set_tab_active(tab)
-
-# -------------------------------------------------------------------------------------------------
-func _process(delta: float) -> void:
-	if _mouse_pressed && _mouse_draging:
-		if OS.window_maximized && Utils.get_native_mouse_position_on_screen().y > 100:
-			OS.window_maximized = false
-			emit_signal("window_demaximized")
-		else:
-			var mouse_pos := get_tree().root.get_viewport().get_mouse_position()
-			var win_pos :=  OS.window_position
-			OS.window_position = mouse_pos + win_pos + _drag_offset
 
 # -------------------------------------------------------------------------------------------------
 func add_tab(name: String, filepath: String) -> UITab:
@@ -73,41 +55,3 @@ func _on_NewFileButton_pressed():
 	var tab := add_tab("Untitled %s" % _file_tabs.get_child_count(), "")
 	set_tab_active(tab)
 
-# -------------------------------------------------------------------------------------------------
-func _on_CloseButton_pressed():
-	emit_signal("close_requested")
-
-# -------------------------------------------------------------------------------------------------
-func _on_MaximizeButton_pressed() -> void:
-	if OS.window_maximized:
-		OS.window_maximized = false
-		emit_signal("window_demaximized")
-	else:
-		OS.window_maximized = true
-		emit_signal("window_maximized")
-
-# -------------------------------------------------------------------------------------------------
-func _on_MinimizeButton_pressed() -> void:
-	OS.window_minimized = true
-	emit_signal("window_minimized")
-
-# -------------------------------------------------------------------------------------------------
-func _on_UITitlebar_gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		if !event.pressed:
-			_mouse_draging = false
-		
-		if event.button_index == BUTTON_LEFT:
-			if event.doubleclick:
-				_on_MaximizeButton_pressed()
-			else:
-				_drag_offset = OS.window_position - (event.global_position + OS.window_position)
-				_mouse_pressed = event.pressed
-	elif event is InputEventMouseMotion:
-		if _mouse_pressed:
-			_mouse_draging = true
-
-# -------------------------------------------------------------------------------------------------
-func _on_Logo_pressed():
-	var popup_panel: PopupMenu = get_node(_menu_popup_path)
-	popup_panel.popup()
