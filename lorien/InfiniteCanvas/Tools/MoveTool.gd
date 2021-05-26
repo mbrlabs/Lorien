9a3fe5121e25f56d0117859f9b5264fd43ac8969
Nicolò Santilio <n.santilio97@gmail.com>
2021-05-26 17:16:35 +0200

    Update MoveTool.gd

--- lorien/InfiniteCanvas/Tools/MoveTool.gd
+++ lorien/InfiniteCanvas/Tools/MoveTool.gd
@@ -1,15 +1,16 @@
 class_name MoveTool
 extends CanvasTool
 
-export (NodePath) var cursor_path : NodePath
-
+# ----------------------------------------------------------------------
 var moving: bool = false setget set_moving, is_moving
 var offset: Vector2
 
+# ----------------------------------------------------------------------
 func _ready():
 	_cursor = get_node(cursor_path)
 	set_enabled(false)
 
+# ----------------------------------------------------------------------
 func set_enabled(e: bool) -> void:
 	.set_enabled(e)
 	if e:
@@ -18,6 +19,7 @@ func set_enabled(e: bool) -> void:
 	else:
 		_cursor.hide()
 
+# ----------------------------------------------------------------------
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		_cursor.global_position = xform_vector2(event.global_position)
@@ -30,6 +32,7 @@ func _input(event: InputEvent) -> void:
 					stroke.set_meta("offset", stroke.position - _cursor.global_position)
 			set_moving(event.is_pressed())
 
+# ----------------------------------------------------------------------
 func _process(delta):
 	if is_moving():
 		var selected_strokes : Array = get_tree().get_nodes_in_group("selected_strokes")
@@ -37,8 +40,10 @@ func _process(delta):
 			for stroke in selected_strokes:
 				stroke.global_position = stroke.get_meta("offset") + _cursor.global_position
 
+# ----------------------------------------------------------------------
 func set_moving(_moving: bool) -> void:
 	moving = _moving
 
+# ----------------------------------------------------------------------
 func is_moving() -> bool:
 	return moving
