d62e1a30dccd4fe9c2379d29ee8b9de0d9e92be1
Nicolò Santilio <n.santilio97@gmail.com>
2021-05-26 16:41:06 +0200

    Update and rename CursorClass.gd to BaseCursor.gd

--- lorien/InfiniteCanvas/Cursor/BaseCursor.gd
+++ lorien/InfiniteCanvas/Cursor/BaseCursor.gd
@@ -1,4 +1,4 @@
-class_name CursorClass
+class_name BaseCursor
 extends Node2D
 
 var _sprite : Sprite
@@ -6,24 +6,13 @@ var _brush_size: int
 var _pressure := 1.0
 
 # --------------------------------------------
-func _ready():
-	pass # Replace with function body.
-
-func _draw():
-	pass
-
-func _input(event):
-	pass
-
-func _process(delta):
-	pass
-
-# --------------------------------------------
 func set_pressure(pressure: float) -> void:
 	pass
 
+# --------------------------------------------
 func change_size(value : int) -> void:
 	pass
 
+# --------------------------------------------
 func _on_zoom_changed(value : float) -> void:
 	pass
