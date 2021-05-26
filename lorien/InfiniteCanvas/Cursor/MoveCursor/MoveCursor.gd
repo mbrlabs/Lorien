18985ba007836f7bfdb8b0e4d31054b33b267dec
Nicolò Santilio <n.santilio97@gmail.com>
2021-05-26 16:53:46 +0200

    Update MoveCursor.gd

--- lorien/InfiniteCanvas/Cursor/MoveCursor/MoveCursor.gd
+++ lorien/InfiniteCanvas/Cursor/MoveCursor/MoveCursor.gd
@@ -1,11 +1,6 @@
 class_name MoveCursor
 extends BaseCursor
 
-
-# -------------------------------------------------------------------------------------------------
-func _ready() -> void:
-	_sprite = $CursorIcon
-
 # -------------------------------------------------------------------------------------------------
 func _on_zoom_changed(zoom_value : float) -> void:
 	_sprite.scale = Vector2.ONE * zoom_value
