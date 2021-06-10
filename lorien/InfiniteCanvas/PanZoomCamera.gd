54d7925cd3e207bcf6d77cba5802e305376ea9b4
Marcus Brummer <mbrlabs7@gmail.com>
2021-06-10 20:04:33 +0200

    Improved zooming

--- lorien/InfiniteCanvas/PanZoomCamera.gd
+++ lorien/InfiniteCanvas/PanZoomCamera.gd
@@ -3,13 +3,15 @@ extends Camera2D
 signal zoom_changed(value)
 signal position_changed(value)
 
-const MAX_ZOOM_LEVEL = 100.0
-const MIN_ZOOM_LEVEL = 0.1
+const MAX_MOUSE_WHEEL_LEVEL := 1000
+const MIN_MOUSE_WHEEL_LEVEL := -9
 const ZOOM_INCREMENT = 0.1
 
 var _current_zoom_level := 1.0
 var _pan_active := false
 var _is_input_enabled := true
+
+var _mouse_wheel_counter := 0
 
 # -------------------------------------------------------------------------------------------------
 func set_zoom_level(zoom_level: float) -> void:
@@ -20,13 +22,14 @@ func set_zoom_level(zoom_level: float) -> void:
 func _input(event: InputEvent) -> void:
 	if _is_input_enabled:
 		if event is InputEventMouseButton:
-			var increment := max(ZOOM_INCREMENT + _current_zoom_level * ZOOM_INCREMENT, ZOOM_INCREMENT)
-			
-			if event.button_index == BUTTON_WHEEL_DOWN:
-				_do_zoom(increment, get_local_mouse_position())
-			elif event.button_index == BUTTON_WHEEL_UP:
-				_do_zoom(-increment, get_local_mouse_position())
-			elif event.button_index == BUTTON_MIDDLE:
+			if event.pressed:
+				if event.button_index == BUTTON_WHEEL_DOWN:
+					_mouse_wheel_counter += 1
+					_do_zoom()
+				elif event.button_index == BUTTON_WHEEL_UP:
+					_mouse_wheel_counter -= 1
+					_do_zoom()
+			if event.button_index == BUTTON_MIDDLE:
 				_pan_active = event.is_pressed()
 		elif event is InputEventMouseMotion:
 			if _pan_active:
@@ -38,24 +41,41 @@ func _do_pan(pan: Vector2) -> void:
 	emit_signal("position_changed", offset)
 
 # -------------------------------------------------------------------------------------------------
-func _do_zoom(incr: float, zoom_anchor: Vector2) -> void:
+func _do_zoom() -> void:
+	var anchor := get_local_mouse_position()
+
 	var old_zoom := _current_zoom_level
-	_current_zoom_level += incr
-	if _current_zoom_level < MIN_ZOOM_LEVEL:
-		_current_zoom_level = MIN_ZOOM_LEVEL
-	elif _current_zoom_level > MAX_ZOOM_LEVEL:
-		_current_zoom_level = MAX_ZOOM_LEVEL
+	if _mouse_wheel_counter <= MIN_MOUSE_WHEEL_LEVEL:
+		_mouse_wheel_counter = MIN_MOUSE_WHEEL_LEVEL
+	elif _mouse_wheel_counter >= MAX_MOUSE_WHEEL_LEVEL:
+		_mouse_wheel_counter = MAX_MOUSE_WHEEL_LEVEL
+	_current_zoom_level = 1.0 + _mouse_wheel_counter * _calc_zoom_increment()
+	
 	if old_zoom == _current_zoom_level:
 		return
 	
-	var zoom_center = zoom_anchor - offset
+	var zoom_center = anchor - offset
 	var ratio = 1.0 - _current_zoom_level/old_zoom
 	offset += zoom_center * ratio
 	
 	zoom = Vector2(_current_zoom_level, _current_zoom_level)
 	emit_signal("zoom_changed", _current_zoom_level)
 
-
+# -------------------------------------------------------------------------------------------------
+func _calc_zoom_increment() -> float:
+	var incr := ZOOM_INCREMENT
+	if _mouse_wheel_counter >= 100:
+		incr *= 5.0
+	elif _mouse_wheel_counter >= 75:
+		incr *= 4.0
+	elif _mouse_wheel_counter >= 50:
+		incr *= 3.0
+	elif _mouse_wheel_counter >= 25:
+		incr *= 2.0
+	elif _mouse_wheel_counter >= 10:
+		incr *= 1.5
+	return incr
+	
 # -------------------------------------------------------------------------------------------------
 func enable_intput() -> void:
 	_is_input_enabled = true
