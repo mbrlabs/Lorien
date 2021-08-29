a45275b14cfb56288cd3aa93b5343e35ebf840ab
Gurchain <gurchain@pop-os.localdomain>
2021-08-28 19:17:15 -0700

    Added core palette functionality

--- lorien/UI/Palette.gd
+++ lorien/UI/Palette.gd
@@ -1,54 +1,62 @@
 extends Control
-
 class_name Palette
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-const DefaultButtonNumber: int = 18
-export var _palette_button: PackedScene = preload("res://UI/Components/PaletteButton.tscn")
-export var _empty_palette_style: StyleBoxFlat = preload("res://UI/Themes/PaletteButton.tres")
+signal color_changed
+
+# -------------------------------------------------------------------------------------------------
+#add const DEFAULT_COLORS array
+
+# -------------------------------------------------------------------------------------------------
+export var _default_button_number: int
+export var _palette_theme: Theme 
+
 onready var _button_grid: GridContainer = $ScrollContainer/GridContainer
-#add default color array for palette
+onready var _color_picker: ColorPicker = get_node("../../PanelContainer/ColorPicker")
+onready var _focused_button: Button
 
+var _palette_button: PackedScene = preload("res://UI/Components/PaletteButton.tscn")
+
+# -------------------------------------------------------------------------------------------------
 func _ready():
-	for i in range(0, DefaultButtonNumber):
+	for btn in range(0, _default_button_number):
 		_button_grid.add_child(_palette_button.instance())
-
-	for i in range(0, DefaultButtonNumber):
-		var _current_button: Button = _button_grid.get_child(i).get_child(0)
-		#print(_current_button.get_path())
-		_current_button.connect("pressed", self, "_on_button_press", [_current_button])	
+		
+		var _current_button: Button = _button_grid.get_child(btn).get_child(0)
+		_current_button.connect("button_down", self, "_on_button_press", [_current_button])
+	
+	_color_picker.connect("color_changed", self, "_on_color_picker_change")
+	_focused_button = _button_grid.get_child(0).get_child(0)
 	
 	#add default colors
-func _on_button_press(var _pressed_button: Node): 
-	if (_pressed_button.get("custom_styles/normal") == _empty_palette_style):
-		print("Hello")
-		#replace empty style with new style, set color to brush color
-	else: 
-		print("Goodbye")
-		#set color picker and brush color to style color 
 
+# -------------------------------------------------------------------------------------------------
+func _on_button_press(var _pressed_button: Button) -> void: 
+	_focused_button = _pressed_button
+	if (_pressed_button.get("custom_styles/normal") == _palette_theme.get("custom_styles/normal")):
+		var normal_style: StyleBoxFlat = _palette_theme.get_stylebox("normal", "Button").duplicate()
+		var focus_style: StyleBoxFlat = _palette_theme.get_stylebox("focus", "Button").duplicate()
+		normal_style.set("bg_color", _color_picker.color)
+		focus_style.set("bg_color", _color_picker.color)
+		_pressed_button.set("custom_styles/normal", normal_style)
+		_pressed_button.set("custom_styles/pressed", normal_style)
+		_pressed_button.set("custom_styles/focus", focus_style)
+		_pressed_button.set("custom_styles/hover", focus_style)
+		
+	else:
+		_set_active_color(_pressed_button)	
 
-	#var _new_button_style = _empty_palette_style.duplicate()
-	#_new_button_style.set_bg_color("#4287f5")
-	#_button_grid.get_child(0).get_child(0).set("custom_styles/normal", _new_button_style)
-# Called when the node enters the scene tree for the first time.
-#func _ready():
-	#for i in range(0,18):
-	#	var _palette_button = PaletteButton.instance()
-	#	_button_grid.add_child(_palette_button)
-	#var _new_button_style = _palette_button_style.duplicate()
-	#_new_button_style.set_bg_color("#bada55")
-	#var _new_button_focus = _new_button_style.duplicate()
-	#_new_button_focus.set_shadow_color("#dee8cf")
-	#_new_button_focus.set_shadow_size(2)
-	#print((_button_grid.get_child(1)).get_child(0).get_path())
-	#((_button_grid.get_child(1)).get_child(0))["custom_styles/normal"].bg_color = Color("#bada55")
-	#((_button_grid.get_child(1)).get_child(0)).set("custom_styles/normal", _new_button_style)
-	#((_button_grid.get_child(1)).get_child(0)).set("custom_styles/hover", _new_button_style)
-	#((_button_grid.get_child(1)).get_child(0)).set("custom_styles/pressed", _new_button_style)
-	#((_button_grid.get_child(1)).get_child(0)).set("custom_styles/focus", _new_button_focus)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# -------------------------------------------------------------------------------------------------
+func _set_active_color(node: Node) -> void:
+	_color_picker.set_pick_color(node.get("custom_styles/normal").bg_color)
+	emit_signal("color_changed", node.get("custom_styles/normal").bg_color)
+	
+# -------------------------------------------------------------------------------------------------
+func _on_color_picker_change(color: Color) -> void:
+	var normal_style: StyleBoxFlat = _focused_button.get_stylebox("normal", "Button")
+	var focus_style: StyleBoxFlat = _focused_button.get_stylebox("normal", "Button")
+	normal_style.set("bg_color", color)
+	focus_style.set("bg_color", color)
+	_focused_button.set("custom_styles/normal", normal_style)
+	_focused_button.set("custom_styles/pressed", normal_style)
+	_focused_button.set("custom_styles/focus", focus_style)
+	_focused_button.set("custom_styles/hover", focus_style)
