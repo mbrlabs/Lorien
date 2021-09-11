3b21ea0523f66f12e0d31ddff4420dc08e2c1b30
Marcus Brummer <mbrlabs7@gmail.com>
2021-09-12 00:19:42 +0200

    Theme overall: most UI elements now have slightly rounded corners

--- lorien/UI/Dialogs/NewPaletteDialog.gd
+++ lorien/UI/Dialogs/NewPaletteDialog.gd
@@ -9,11 +9,12 @@ onready var _line_edit: LineEdit = $MarginContainer/Container/LineEdit
 # -------------------------------------------------------------------------------------------------
 func _on_SaveButton_pressed() -> void:
 	var name := _line_edit.text
-	var palette := PaletteManager.create_custom_palette(name)
-	if palette != null:
-		PaletteManager.save()
-		hide()
-		emit_signal("new_palette_created", palette)
+	if !name.empty():
+		var palette := PaletteManager.create_custom_palette(name)
+		if palette != null:
+			PaletteManager.save()
+			hide()
+			emit_signal("new_palette_created", palette)
 
 # -------------------------------------------------------------------------------------------------
 func _on_CancelButton_pressed() -> void:
