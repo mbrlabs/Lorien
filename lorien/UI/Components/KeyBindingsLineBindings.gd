33a5b92e06876789d01aaaa0fe8d06e471dec198
Paul Konstantin Gerke <paulkgerke@craftware.info>
2022-07-01 22:41:52 +0200

    Add trashcan to icons, revert red background

--- lorien/UI/Components/KeyBindingsLineBindings.gd
+++ lorien/UI/Components/KeyBindingsLineBindings.gd
@@ -4,40 +4,26 @@ extends HBoxContainer
 signal modified_binding(bindings_data)
 
 # -------------------------------------------------------------------------------------------------
-var bindings_data := {}
+var _bindings_data := {}
+var _preloaded_image := preload("res://Assets/Icons/trashcan.png")
 
 # -------------------------------------------------------------------------------------------------
 # Keybindings data: {"action": "str", "readable_name": "str", "events": [...]}
-func set_keybindings_data(_bindings_data: Dictionary) -> void:
+func set_keybindings_data(bindings_data: Dictionary) -> void:
 	for child in get_children():
 		remove_child(child)
 
-	var ref_stylebox: StyleBox = Button.new().get_stylebox("normal").duplicate(true)
-	var bg_stylebox: StyleBox = StyleBoxFlat.new()
-	bg_stylebox.content_margin_top = ref_stylebox.content_margin_top
-	bg_stylebox.content_margin_bottom = ref_stylebox.content_margin_bottom
-	bg_stylebox.content_margin_left = ref_stylebox.content_margin_left
-	bg_stylebox.content_margin_right = ref_stylebox.content_margin_right
-	
-	var red_bg := bg_stylebox.duplicate(false)
-	red_bg.bg_color = Color.red
-	var lightred_bg := bg_stylebox.duplicate(false)
-	lightred_bg.bg_color = Color.lightcoral
-
-	bindings_data = _bindings_data
+	_bindings_data = bindings_data
 	for event in bindings_data["events"]:
 		if event is InputEventKey:
 			var remove_button = Button.new()
 			remove_button.text = OS.get_scancode_string(event.get_scancode_with_modifiers())
-			
-			remove_button.add_stylebox_override("normal", red_bg)
-			remove_button.add_stylebox_override("pressed", red_bg)
-			remove_button.add_stylebox_override("hover", red_bg)
-			remove_button.add_stylebox_override("focus", lightred_bg)
+			remove_button.icon = _preloaded_image
+	
 			remove_button.connect("pressed", self, "_remove_pressed", [event])
 			add_child(remove_button)
 
 # -------------------------------------------------------------------------------------------------
 func _remove_pressed(event: InputEvent) -> void:
-	bindings_data["events"].erase(event)
-	emit_signal("modified_binding", bindings_data)
+	_bindings_data["events"].erase(event)
+	emit_signal("modified_binding", _bindings_data)
