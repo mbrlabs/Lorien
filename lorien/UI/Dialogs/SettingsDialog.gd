32e3411b16d736be8df3ea001e4a77443df69238
Gurchain <89324754+Gurchain@users.noreply.github.com>
2022-08-27 22:36:37 -0700

    Larger range for calculated ui min and max scales

--- lorien/UI/Dialogs/SettingsDialog.gd
+++ lorien/UI/Dialogs/SettingsDialog.gd
@@ -113,9 +113,10 @@ func _set_languages(current_locale: String) -> void:
 
 #--------------------------------------------------------------------------------------------------
 func _set_UIScale_range():
-	var screen_scale: float = (OS.get_screen_size().x * OS.get_screen_size().y) / (ProjectSettings.get_setting("display/window/size/width") * ProjectSettings.get_setting("display/window/size/height"))
-	_ui_scale.min_value = screen_scale / 2
-	_ui_scale.max_value = screen_scale + 1.5
+	var screen_scale_max: float = (OS.get_screen_size().x * OS.get_screen_size().y) / (ProjectSettings.get_setting("display/window/size/width") * ProjectSettings.get_setting("display/window/size/height"))
+	var screen_scale_min: float = max(OS.get_screen_size().x, OS.get_screen_size().y) / ProjectSettings.get_setting("display/window/size/width")
+	_ui_scale.min_value = max(screen_scale_min / 2, 0.5)
+	_ui_scale.max_value = screen_scale_max + 1.5
 
 #--------------------------------------------------------------------------------------------------
 func get_max_ui_scale() -> float:
