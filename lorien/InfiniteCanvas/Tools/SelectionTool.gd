mbrlabs/Lorien

commit bc83fbe6691c0879335d0f2577443bad076f47f3

Reset state of tool being switched (#155)
Fixes (#153) "Segmentation fault when switching modes mid-stroke"

lorien/InfiniteCanvas/Tools/SelectionTool.gd
@@ -245,3 +245,11 @@ func get_selected_strokes() -> Array:
 func _on_brush_color_changed(color: Color) -> void:
 	var strokes := get_selected_strokes()
 	_modify_strokes_colors(strokes, color)
+
+# ------------------------------------------------------------------------------------------------
+func reset() -> void:
+	_state = State.NONE
+	_selection_rectangle.reset()
+	_selection_rectangle.update()
+	_commit_strokes_under_selection_rectangle()
+	deselect_all_strokes()

lorien/InfiniteCanvas/InfiniteCanvas.gd
@@ -71,8 +71,8 @@ func center_to_mouse() -> void:
 # -------------------------------------------------------------------------------------------------
 func use_tool(tool_type: int) -> void:
 	_active_tool.enabled = false
-	_selection_tool.deselect_all_strokes()
-	
+	var prev_tool := _active_tool
+
 	match tool_type:
 		Types.Tool.BRUSH:
 			_active_tool = _brush_tool
@@ -92,7 +92,9 @@ func use_tool(tool_type: int) -> void:
 		Types.Tool.SELECT:
 			_active_tool = _selection_tool
 			_use_optimizer = false
-			
+
+	if prev_tool != _active_tool:
+		prev_tool.reset()
 	_active_tool.enabled = true
 	_active_tool.get_cursor()._on_zoom_changed(_camera.zoom.x)
 

lorien/InfiniteCanvas/Tools/CanvasTool.gd
@@ -89,3 +89,7 @@ func end_stroke() -> void:
 # -------------------------------------------------------------------------------------------------
 func xform_vector2(v: Vector2) -> Vector2:
 	return _canvas.get_camera().xform(v)
+
+# -------------------------------------------------------------------------------------------------
+func reset() -> void:
+	end_stroke()

lorien/InfiniteCanvas/Tools/EraserTool.gd
@@ -22,8 +22,7 @@ func _input(event: InputEvent) -> void:
 					_update_bounding_boxes()
 				performing_stroke = true
 			elif !event.pressed:
-				_bounding_box_cache.clear()
-				performing_stroke = false
+				reset()
 
 # -------------------------------------------------------------------------------------------------
 func _process(delta: float) -> void:
@@ -73,3 +72,8 @@ func _update_bounding_boxes() -> void:
 	var strokes: Array = _canvas.get_all_strokes()
 	_bounding_box_cache = Utils.calculte_bounding_boxes(strokes, BOUNDING_BOX_MARGIN)
 	#$"../Viewport/DebugDraw".set_bounding_boxes(_bounding_box_cache.values())
+
+# ------------------------------------------------------------------------------------------------
+func reset() -> void:
+	_bounding_box_cache.clear()
+	performing_stroke = false

commit 1d53693f48caf4f9391461c2707e0031413a9f69

SelectionTool: fix missing cursor on initial usage
Fixes missing on-canvas cursor when switching to SelectionTool for the
first time.

Fixes: bc83fbe6691c (`Reset state of tool being switched (#155)`)

lorien/InfiniteCanvas/Tools/SelectionTool.gd
@@ -33,6 +33,7 @@ var _bounding_box_cache = {} # BrushStroke -> Rect2
 # ------------------------------------------------------------------------------------------------
 func _ready():
 	_selection_rectangle = get_node(selection_rectangle_path)
+	_cursor.mode = SelectionCursor.Mode.SELECT
 
 # ------------------------------------------------------------------------------------------------
 func tool_event(event: InputEvent) -> void: