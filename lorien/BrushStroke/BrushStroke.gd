83b425a940c1cc31ab1ea6272c949768633e559f
Romain Petit <romain.petit@whoz.com>
2023-07-11 15:25:36 +0200

    selection - cursor at first time and deselect by clicking outside the selection box - cleaning

--- lorien/BrushStroke/BrushStroke.gd
+++ lorien/BrushStroke/BrushStroke.gd
@@ -25,7 +25,6 @@ var pressures: Array # Array<float>
 var top_left_pos: Vector2
 var bottom_right_pos: Vector2
 var _box_size : Vector2
-var layer: int
 
 # ------------------------------------------------------------------------------------------------
 func _ready():
