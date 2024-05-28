b12b7cb84a34a1c336da458086bc5b34b1914dbb
Freddie Haddad <freddie.haddad@outlook.com>
2024-05-28 16:34:50 -0700

    feat: Add option to render color palette at mouse
    This feature introduces a new setting in the general tab giving the user
    the option to render the color palette at the toolbar (the existing
    behavior) or at the mouse cursor (new behavior).  This is particularly
    helpful when using a key bind to toggle the color palette UI.

--- lorien/Config.gd
+++ lorien/Config.gd
@@ -25,4 +25,5 @@ const DEFAULT_UI_SCALE  			:= 1.0
 const DEFAULT_GRID_PATTERN 			:= Types.GridPattern.DOTS
 const DEFAULT_GRID_SIZE 			:= 25.0
 const DEFAULT_TOOL_PRESSURE			:= 0.5
+const DEFAULT_PALETTE_LOCATION		:= Types.PaletteLocation.TOOLBAR
 
