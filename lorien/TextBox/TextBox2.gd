4ea4109469e60d9f0dfe022a3bdd325e9d8cdf27
Marco Eibensteiner <marco_meier_6@hotmail.com>
2025-09-24 15:42:13 +0200

    Added changing_theme to the Text Box Editor

--- lorien/TextBox/TextBox2.gd
+++ lorien/TextBox/TextBox2.gd
@@ -5,11 +5,16 @@ var textEdit : TextEdit = $Panel/Panel/VBoxContainer/TextEdit
 @onready
 var saveButton : Button = $Panel/Panel/VBoxContainer/HBoxContainer/SaveButton
 var textBox : TextBox
-
+@onready
+var panel : Panel = $Panel/Panel
 signal textBox_ok
 signal textBox_cancel
 signal textBox_ok_update
 var label_position : Vector2
+
+
+func _ready() -> void:
+	Settings.changed_theme.connect(_on_theme_changed)
 
 func _on_save_button_pressed() -> void:
 	if textBox == null:
@@ -43,7 +48,13 @@ func _on_panel_close_click() -> void:
 	textEdit.text = ""
 	visible = false
 
+func _on_theme_changed(themeName : String) -> void:
+	var themePath : String = str("res://UI/Themes/", themeName, "/theme.tres")
+	var currentTheme : Theme = load(themePath)
+	theme = currentTheme
 
 func _on_visibility_changed() -> void:
+	
+	
 	if visible:
 		textEdit.grab_focus()
